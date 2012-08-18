7863626b51ccbad3073f43bbfb74e524c9cc7517
Brian Kirsten <brian.kirsten.bss@gmail.com>
2012-08-18 11:50:23 -0400

    initial check in

---
+++ src/ol_split_books.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import unicodedata
+import json
+
+for line in sys.stdin:
+	line = line.strip()
+	try:
+		jsonStr = line
+		data = json.loads(jsonStr)
+		title = data["title"].encode('UTF-8')
+		if( title[0:1].lower() == 'l'):
+			print jsonStr.encode('UTF-8')
+	except:
+		continue
+							
