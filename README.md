3b4914c580c9c45757f8fac42350cc3b95180a38
Brian Kirsten <brian.kirsten.bss@gmail.com>
2012-08-18 12:02:29 -0400

    Change to add command line argument and README

---
+++ README.md
@@ -0,0 +1,6 @@
+OpenLibrary Map/Reduce Scripts
+==============================
+
+A variety of Map/Reduce scripts to be used against OpenLibrary data [1]
+
+[1]: http://openlibrary.org/developers/dumps
--- src/ol_split_books.py
+++ src/ol_split_books.py
@@ -4,13 +4,15 @@ import sys
 import unicodedata
 import json
 
+letter = sys.argv[1]
+
 for line in sys.stdin:
 	line = line.strip()
 	try:
 		jsonStr = line
 		data = json.loads(jsonStr)
 		title = data["title"].encode('UTF-8')
-		if( title[0:1].lower() == 'l'):
+		if( title[0:1].lower() == letter):
 			print jsonStr.encode('UTF-8')
 	except:
 		continue
